240701bebc68e1c1620895da51a22ee9fa6f5e78
Jérémy Moreau <jeremyfs.moreau@gmail.com>
2017-10-13 23:42:45 +0200

    tp alarme

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cognitiveservices = require('botbuilder-cognitiveservices');
 // setup restify
 
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3990, function() {
+server.listen(process.env.port || process.env.PORT || 3989, function() {
     console.log('% bot started at %', server.name, server.url)
 });
 
@@ -19,7 +19,7 @@ var connector = new botbuilder.ChatConnector({
 server.post('/api/messages', connector.listen());
 
 // Reply by echoing
-var bot = new botbuilder.UniversalBot(connector, function(session) {    
+var bot = new botbuilder.UniversalBot(connector, function(session) { 
     return session.beginDialog('alarme');    
 });
 bot.dialog('alarme',require('./dialogs/alarm'));
--- dialogs/alarm.js
+++ dialogs/alarm.js
@@ -2,35 +2,107 @@ var builder = require('botbuilder');
 
 module.exports = [
     function (session, args, next) {
-        session.dialogData.alarm = args || {};
-        builder.Prompts.choice(session, "Bienveue dans la gestion de votre alarme. <br/>Que souhaitez-vous faire ?", "Ajouter une alarme|Lister les alarmes à venir|Historique de mes alarmes", { listStyle: builder.ListStyle.button });
+        if (!session.userData.everyAlarms)
+            session.userData.everyAlarms = [];
+        builder.Prompts.choice(session, "Bienveue dans la gestion de votre alarme. <br/>Que souhaitez-vous faire ?", "Ajouter une alarme|Lister les alarmes|Historique de mes alarmes", { listStyle: builder.ListStyle.button });
     },
     function (session, results, next) {
         if (results.response.index == 0 ) {
-            //session.dialogData.alarm.name = results.response;
-            builder.Prompts.time(session, "What time would you like to set an alarm for?");
+            session.dialogData.newAlarm = {};
+            builder.Prompts.text(session, "Indiquez un nom pour l'alarme");
         } 
-        else if (results.response.index == 1 ) {
-            session.send('Les alarmes à venir :');
-            next();
+        else if (results.response.index == 1 ) 
+        {
+            if(session.userData.everyAlarms.length >=1)
+            {
+                var message = new builder.Message(session);
+                message.attachmentLayout(builder.AttachmentLayout.carousel);
+                var richCards = [];
+                for (var i = 0; i<session.userData.everyAlarms.length; i++) {
+                    var checkDate = Date.parse(session.userData.everyAlarms[i].date);
+                    console.log("?????????"+ checkDate);
+                    console.log("!!!!!!!!!!!!" + Date.now());
+                    if(checkDate > Date.now())
+                    {
+                        var number = i+1;
+                        var richCard =  new builder.HeroCard(session)
+                                        .title("Alarme " + number)
+                                        .subtitle("Nom : " + session.userData.everyAlarms[i].name)
+                                        .text("Jour : " + session.userData.everyAlarms[i].date)
+                        richCards.push(richCard)
+                    }
+                }
+                if(richCards.length > 0)
+                {
+                    message.attachments(richCards);
+                    session.send(message).endDialog();
+                }
+                else
+                {
+                    session.send("Pas d'alarme");
+                }
+            }
+            else
+            {
+                session.send("Pas d'alarme");
+            }
         }
-        else if (results.response.index == 2 ) {
-            session.send('L\'historique des alarmes : ');
+        else if (results.response.index == 2 ) 
+        {
+            if(session.userData.everyAlarms.length >=1)
+            {
+                var message = new builder.Message(session);
+                message.attachmentLayout(builder.AttachmentLayout.carousel);
+                var richCards = [];
+                for (var i = 0; i<session.userData.everyAlarms.length; i++) {
+                    var checkDate = Date.parse(session.userData.everyAlarms[i].date);
+                    console.log("?????????"+ checkDate);
+                    console.log("!!!!!!!!!!!!" + Date.now());
+                    if(checkDate < Date.now())
+                    {
+                        var number = i+1;
+                        var richCard =  new builder.HeroCard(session)
+                                        .title("Alarme " + number)
+                                        .subtitle("Nom : " + session.userData.everyAlarms[i].name)
+                                        .text("Jour : " + session.userData.everyAlarms[i].date)
+                        richCards.push(richCard)
+                    }
+                }
+                if(richCards.length > 0)
+                {
+                    message.attachments(richCards);
+                    session.send(message).endDialog();
+                }
+                else
+                {
+                    session.send("Pas d'alarme");
+                }
+            }
+            else
+            {
+                session.send("Pas d'alarme");
+            }
+        }
+    },
+    function (session, results, next) {
+        if (results.response) {
+            session.dialogData.newAlarm.name = results.response;
+        }
+        if (!session.dialogData.newAlarm.date) {
+            builder.Prompts.time(session, "Renseignez la date et l'heure du jour auquel vous souhaitez être réveillé (Format : MM/JJ/AAAA HH:MM");
+        } else {
             next();
         }
     },
     function (session, results) {
-        session.send('test');
-    //     session.dialogData.reservation.duration = results.response;
-
-    //    // Process request and display reservation details
-    //     session.send(`Reservation confirmée. Détails de la réservation: <br/>
-    //         Nom: ${session.dialogData.reservation.person.name} <br/>
-    //         Email: ${session.dialogData.reservation.person.email} <br/>
-    //         Age de la personne effectuant la réservation: ${session.dialogData.reservation.person.age} <br/>
-    //         Destination : ${session.dialogData.reservation.destination} <br/>
-    //         Date check-in : ${session.dialogData.reservation.date} <br/>
-    //         Nombre de nuits : ${session.dialogData.reservation.duration}`);
-         session.endDialog();
-    }
-];
+        if (results.response) {
+            session.dialogData.newAlarm.date = builder.EntityRecognizer.resolveTime([results.response]);
+            session.userData.everyAlarms.push(session.dialogData.newAlarm);
+            session.send("L'alarme a bien été enregistrée");
+        }
+        session.endDialogWithResult({ response: session.dialogData });
+    },
+    function (session, results) {
+        session.dialogData = results.response; 
+    }    
+]
